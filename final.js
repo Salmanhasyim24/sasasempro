170bdaf71af534abb226a43a8efa5bcced689ca5
Salman Hasyim Adz Dzikri <98438105+Salmanhasyim24@users.noreply.github.com>
2026-04-14 13:20:43 +0700

    Update

---
+++ final.js
@@ -0,0 +1,207 @@
+const text = `disini aku menulis pada 12 April, setelah pulang dari soreang 
+
+jika sasa ada waktu luang, boleh di baca ya? terima kasih
+
+aku tau sasa pasti ada 100 alasan untuk menjauh dari aku di awal awalnya pertemuan kita, tapi di awal pertemuan kita juga.. sasa memberanikan diri bahwa ada 101 alasan untuk bertahan sama aku... aku bangga padamu, sa, bangga sekali
+terima kasih udah banyak ngalahnya ke banyak hal dan tentang segalanya, sa 
+jujur, aku mau bilang terima kasih dan meminta maaf secara langsung ke sasa
+kalimat ini bukan sebagai penghibur, aku ngelakuin keduanya buat mewakili mereka semua sampe ngga bisa nyamperin itu sendiri ke sasa 
+sasa membanggakan, sasa diterima, sasa diapresiasi
+atas semua hal kecil, atas semua hal yang mungkin remeh yang bahkan.... sasa sendiri ngga sadar, sasa jelas worth and deserve 
+thank u and i'm proud of u, sasaa 
+
+terakhir kali aku lihat sasa itu di kampus saat sasa beli takjil (tentunya sasa dengan teman sasa), disitu sasa sangat lepas, aku lihat sasa tuh bahagia, tenang, baik baik saja dan aku bangga dan senang lihat sasa seperti itu 
+satu satunya yang terlintas saat itu mungkin sampe sekarang cuma... i want to give u a hug really long hug, saa
+may sal hug sasa again, please? 
+
+terlepas dari apapun yang udah sasa lalui dan sedang sasa rasakan, sasa kuat dan sasa hebat, makasih udah nunjukin ke aku gimana kuatnya sasa terakhir kalinya
+keputusan sasa untuk meninggalkanku mungkin cara yang tepat, dan tentunya ngga boleh aku tangisi
+aku ingat ucapan sasa sebelum menutup semua jalanku menuju hidup sasa dan ucapan ini selalu sasa lontarkan ke aku, sasa bilang: "you deserve better, sal".
+tapi yang kupikir pada saat itu, kenapa bukan kita yang berubah menjadi lebih baik?
+
+hingga saat ini, yaa saat ini, aku masih menunggu doa itu dikabulkan
+keep going n keep growing, i hope there is another life we could still together? i hope
+
+aku mau liat senyum terlebarnya sasa dan aku mau kalo sasa itu orangnya
+aku yakin karena sasa yakinin aku, dengan nunjukin usahanya sasa kemaren buat aku
+jadi aku bisa yakin bukan karena sekedar sayang sama sasa, tapi karena ada usaha yang sasa kasih ke aku
+
+aku hanya berharap wanita manis ku ini berhasil mencapai semua impian di hidupnya, serta dilancarkan segala urusannya
+semoga semesta selalu memberimu kemudahan atas segala usahamu, semoga doamu selalu terkabulkan satu persatu, semoga setiap langkah selalu di ringankan, semoga pundakmu dikuatkan, semoga segala urusanmu dipermudah, semoga hatimu dilapangkan atas segala takdir yang diluar rencanamu, dan semoga sasa selalu dikelilingi orang orang baik dimanapun sasa berada
+aku akan jadi orang yang selalu bangga pada sasa 
+
+tentang semua yang pernah kita lewati, aku menyimpannya sebagai sesuatu yang baik termasuk hal-hal kecil yang pernah sasa ucapkan dan berikan, itu tetap aku ingat sebagai bagian dari kenangan yang berarti
+
+ke depan, aku akan fokus memperbaiki diri dan melanjutkan langkahku dengan lebih baik (kedewasaan, emosi, value dan yang lainnya) bukan untuk siapa siapa, tapi untuk menjadi versi yang lebih matang dari diriku sendiri 
+
+aku juga ingin sasa tau, aku benar benar berharap sasa menemukan kebahagiaan dengan jalanmu sendiri, dengan seseorang atau hal hal yang bisa membuat sasa merasa aman dan dipahami
+
+aku kepikiran, komunikasi itu mungkin bisa di bilang penting, tetapi dengan memahami perasaan seseorang itu jauh lebih penting. kenapa? bukan tidak sayang, tetapi karena dengan kita memahami semuanya, kita akan tau apa yang perlu kita lakukan kan?
+
+tidak ada yang perlu sasa khawatirkan dari aku, sa. aku akan baik-baik saja
+
+ada 2 pesan sasa yang buat aku mempertanyakan isi pesannya itu hingga saat ini, pesan pertama sasa bilang bahwa sasa belom selesai (terjebak di hubungan masa pahit yang lalu itu) dan satu pesan yang mungkin sedikit mengecewakan aku yang sasa bilang "i heard that you have a partner beside me and diandra at the same time, and you already with her since early semester.", jujur aku sampe sekarang masih memikirkan pesan yang sasa kirimkan itu, kenapa sa? kenapaa? ucapan atau denger pernyataan dari orang lain perihal aku bisa seperti itu, jahatt
+
+aku mempunyai hubungan dengan seseorang selain sasa dan yang lalu? aku yakin sasa ngga salah tulis, karena aku yakin sebelum mengirim pesan itu sasa pasti mempertimbangkan semuanya
+
+aku dengan yang lalu itu masa lalu aku, ngga bisa berubah masa lalu itu, tapi bukannya bisa berubah ke yang lebih baik?
+aku juga merasa cemburu kalo sasa bicara tentang masa lalu sasa, makanya aku ngga mau ada obrolan tentang masa lalu kita, itu menyakitkan untuk kita kan? 
+
+mungkin aku akan memahaminya kalo pesan sasa yang kedua itu agar menyelesaikan hubungan yang kita bangun
+maaf sebelumnya, tapi kenapa dengan cara seperti itu, sa? untuk mengakhirinya
+
+aku pernah kepikiran juga, apakah kemaren di saat waktu intens (selalu telpon sasa, selalu ada buat sasa, pokoknya yang berkaitan hubungan aku dengan sasa lebih deket) itu sasa ngga nyaman dan merasa terkekang ya?
+saat itu aku ingin mengerti sasa tetapi malah menambah beban untuk sasa, maaf sasa, saat itu aku ngga peka dan ngga mengerti 
+
+kalo boleh tau, apakah dengan aku terlalu intens ke sasa itu sasa merasa bahwa sasa itu terasa tertekan dan kepikiran kalo aku terlalu deket dengan sasa, sasa ketergantungan sama aku dan itu membuat sasa menjauhi aku? jangan berpikiran seperti itu ya, sasa
+
+sasa... sasa butuh ruang apa sasa takut dan masih trauma dengan masa lalu sasa? apa takut dengan perasaan sasa sendiri...
+kemaren saat bersama aku sasa belom merasa aman dan tenang ya? dan itu yang membuat sasa takut ke aku? heii sasaa.... aku pernah berpikir juga, apa sasa dengan menjauh itu solusi yang paling tepat? dengerr, itu cuma cara sasa kabur dari rasa, sasa ngga kehilangan perasaan, sasa cuma takut menghadapinya
+sasa pantas dicintai, aku disini, aku di dekat sasa, jangan pernah berpikir dan merasa ngga dicintai yaa?
+
+aku pernah kepikiran juga, apa jangan jangan belom siap untuk ada komitmen? ya karena ngerasa ada yang kurang dan masih trauma karena hubungan sebelumnya
+ngga mau kehilangan kontrol, sebab jika sudah memutuskan untuk berkomitmen, itu akan susah mengontrol diri 
+apa karena aku selalu ada ya? apakah ini menekan sasa ya, jika aku selalu ada? maaf yaa
+
+sasa pernah denger film "can this love be translated?" yang diperankan pacarku, yaitu go youn-jung ada upama dimana: “menempel karena ingin di cintai, lalu kabur karena merasa tak layak dicintai, tapi berharap kau tak membencinya" atau "cara melarikan diri paling aman untuk seseorang yang ngga pernah dicintai adalah meninggalkan semua hal yang dicintainya", tolong jangan berpikiran seperti ini ya?
+
+kalau semua orang punya bahasa cintanya masing-masing
+mungkin ada yang bahasanya itu pake tanda titik, biar terlihat (tegas dan jelas) tapi ada juga yang pake tanda tanya (muter muter dan kurang jelas maunya apa)
+dan dari semuanya kalau kita beneran sayang sama mereka dan meskipun terlihat seperti hal yang sulit, kenapa ngga coba dan berusaha untuk memahami bahasanya daripada menyerah dan pergi begitu saja?
+semoga kita juga dipertemukan sama orang-orang yang mau berusaha berempati, mengerti, dan memahami bahasa cinta kita
+
+sasa ngga perlu minta maaf, aku ngerti kemaren sasa sedang cape dan itu membuat pikiran sasa ovt ya? aku sedih dengernya, sedih 
+aku ngga bisa mengerti dan pikiran itu yang membuat sasa sendiri harus minta maaf dan sempet ovt, ngga boleh ada kepikiran untuk minta maaf karena ovt ya? aku yang seharusnya minta maaf jadi penyebab ovt nya sasa 
+
+yang aku pelajari dari hubungan kita dan akan aku terapkan kedepannya itu perihal cinta, cinta dari kedua belah pihak itu terdengar penting, tetapi ada yang lebih penting seperti diusahakan, dipahami, dimengerti, dan point yang ngga kalah penting itu saling menjaga mentalnya (maaf kemaren aku tidak seperti ini, sa), harusnya itu kemaren aku memahaminya, bahwa dibuat tenang dan nyaman dalam suatu hubungan itu point utamanya 
+
+pernah aku ucapkan sebelumnya juga, kita proses bareng bareng, sa
+semua bisa kita perbaiki, kita sembuh perlahan bersama juga
+asalkan sasa dan tentunya aku percaya bahwa kita itu pasti bisa ngejalaninnya bareng bareng
+maaf aku mengatakan ini, sasa 
+sasa ngga hanya menyakiti aku tapi apa sasa sempat kepikiran kalo itu juga menyakiti perasaannya sasa sendiri? 
+yang pernah aku bilang ke sasa, kita belajar perlahan secara bersama-sama, sa, aku disini bersama sasa, aku ngga ninggalin sasa
+
+mungkin saat itu juga, sasa ngerasa cape ya? apalagi aku sering meminta sasa untuk menemaniku saat itu, aku ngga akan ganggu sasa lagi, pastinya sasa tidak nyaman dengan keberadaan aku
+
+sasaa, aku baru tau sasa seseorang yang kadang butuh ruang, dan aku ingin sasa tau bahwa aku mengerti
+aku ngga marah padamu, sa 
+dan aku ngga menyalahkanmu atas cara sasa menghadapi berbagai hal
+aku hanya ingin memahami sasa lebih baik, alih-alih memaksamu menjadi seperti yang aku harapkan dari hubungan kemaren
+bahkan saat sasa menarik diri, menjadi diam, atau bisa terlihat jauh, aku tetap di sini menemani kan? tapi bentuk menemani seharian itu terkadang akan salah (aku kemaren aneh ya? setiap orang punya hidupnya masing masing, kenapa aku seperti itu) malah ngebebanin seseorang saat aku di bawah
+
+aku jadi yakin kalo semua orang punya bahasa itu sendiri, sesuai dengan penduduk yang ada di dunia ini..
+sebab, setiap orang berbicara dalam bahasanya sendiri (makanya ada namanya salah paham, menyinggung, dan lainnya)
+pasti bikin prutasi juga kan, tetapi dengan memahaminya dan mempelajarinya, seperti kata katanya, susunan kata katanya, tanda bacanya, pasti seseorang itu dapat mengerti lawan bicaranya 
+
+aku melihat sasa yang sedang berusaha dengan cara sasa sendiri, meski tidak selalu terlihat jelas dilihat olehku (ada beberapa yang terasa oleh aku)
+aku mengerti bahwa kadang emosi terasa sangat berat bagi sasa, dan jarak terasa lebih aman, dan meski terkadang menyakitkan bagiku, aku ngga menyimpannya sebagai kesalahannya sasa 
+sasa mungkin ngga selalu menyadarinya, tapi sasa kuat, lebih kuat dari yang sasa kira
+dan sasa pantas dicintai sepenuhnya, ngga hanya untuk bagian bagian mudah darimu, tapi juga untuk bagian bagian yang sasa sedang perjuangkan
+kemaren aku sedang memahami setiap bagian darimu seperti bagaimana sasa mencintai, bagaimana sasa mengekspresikan hal hal, dan bagaimana sasa perlu dicintai
+aku hanya ingin mencintaimu dengan cara yang terasa tepat bagimu
+tetapi ada kesalahan yang membuat hubungan itu berakhir, karena aku terburu buru untuk menyelesaikan dan itu membuat sasa ngga nyaman
+maaf, kemaren ego dan emosi aku ngga kena
+aku kalah
+
+maaf, aku malah membuat opini yang aku suarakan seperti diatas (maaf, aku ingin mensuarakan isi hati aku)
+
+no matter what happens sasa
+sasa ngga sempurna, aku juga, sa
+sasa jangan anggep seseorang sebagai manusia sempurna, sasa jangan merasa kalo sasa ngga pantes buat siapapun 
+karena kita juga manusia, kita punya kekurangan masing masing dan aku harap sasa dengan seseorang itu bisa saling melengkapi 
+ 
+kalo boleh aku minta tolong ke sasa..
+tolong tetap tumbuh dan berkembang yaa? boleh berdiri sebentar dan ke cermin, sebentar aja? 
+heii saa, lihat
+lihat diri sasa di cermin, bukankah sasa sudah sangat hebat?
+jangan lupa untuk bilang terimakasih sama diri sasa sendiri yang sudah kuat sampai sejauh ini
+percayalah semua akan baik baik saja, semua yang sedang sasa usahakan satu persatu pasti akan Tuhan iyakan
+
+dan soal berubah ke yang lebih baik dan berani itu harus kan, sa? aku bangga padamu, sa 
+
+banyak yang ingin aku tanyakan kepadamu, sa
+banyak juga yang ingin aku sampaikan kepada sasa 
+
+but I'm letting go
+
+semoga sasa dipertemukan sama orang yang jauh lebik baik dari aku, seseorang yang bener bener bisa memahami sasa, menghargai perasaanmu, dan membuatmu merasa aman tanpa harus menjelaskan banyak hal ya
+maaf buat semua kesalahan aku yang ngga bisa aku tebus sampe saat ini dan maaf buat janji janji yang ngga bisa aku tepatin sampe hari ini
+
+aku baru sadar ternyata sifat aku selama ini bener bener buat sasa cape tapi sasa tetap bertahan sama aku
+
+sasa baik baik kedepan nya yaa, makasi ya udah ngasi rasa di setiap notif yang sasa beri, makasih juga udah mau kenal pribadi aku yang ngga semua orang tau, aku gagal dan hari ini aku berusaha ikhlas untuk semuanya, maafin aku, aku uda berusaha sebaik mungkin
+sasa, lanjutin perjalanan sasa tanpa adanya aku. jaga diri kmu dan hidup bahagia setelah terlepas dari hari buruk sasa sama aku
+
+jika suatu hari sasa berpikiran menyesal telah mengenalku, jika namaku justru menjadi bagian dari hal hal yang ingin sasa lupakan, percayalah… itu bukan hal yang ngga pernah kupikirkan
+aku sadar, mungkin kehadiranku kemaren bukan membawa bahagia, melainkan luka mungkin aku datang dengan niat baik, tapi caraku salah
+aku ingin menjadi seseorang yang berarti, tapi tanpa sadar justru menjadi beban dalam hidupnya sasa 
+aku tau, aku mungkin orang terburuk yang pernah sasa temui
+sering membuat sasa kecewa dengan respon yang aku berikan, dan jarang mengerti saat sasa butuh dimengerti
+ada kata kata yang seharusnya ngga kuucapkan, sikap yang seharusnya ngga kutunjukkan, perlakuan yang aku berikan, dan perasaan sasa yang seharusnya lebih kujaga
+namun aku telat menyadarinya, saat semuanya sudah terlanjur meninggalkan bekas
+
+jika kehadiranku hanya mengajarkanmu tentang lelah, tentang sabar yang dipaksakan, dan tentang luka yang harus sasa sembuhkan sendiri, aku benar-benar minta maaf, sa 
+
+jujur aku ingin minta maaf secara langsung ke sasa, tapi aku tau, sasa ngga mau ketemu aku lagi
+
+maaf karena tidak bisa menjadi versi terbaik di saat sasa membutuhkannya
+maaf karena mencintaimu dengan caraku yang berantakan
+maaf karena membuat sasa bertanya, “kenapa harus bertemu dengan sal?”
+aku hanya ingin sasa tau, penyesalanku itu nyata
+jika aku bisa memilih, aku ingin dikenang sebagai pelajaran, bukan sebagai luka (nyatanya aku sebagai luka buat sasa, bukan tempat nyamannya sasa)
+
+dan jika memang aku harus pergi dari ceritamu, biarlah kepergianku menjadi permintaan maaf paling tulus yang pernah kuberikan ya? aku ingin kejar mimpi dulu, sebelum aku memantaskan keberadaan aku, aku trauma
+
+aku ngga nyesel dipertemukan dengan sasa, aku bahkan bersyukur bisa dipertemukan dengan sasa, setidaknya aku pernah ngerasain apa yang dinamakan dengan kasih sayang dan dicintai begitu hebatnya yang sasa berikan 
+
+maaf beberapa bulan setelah lebaran aku tidak menghubungi sasa, kalo boleh cerita ke sasa
+kemaren di akhir bulan maret, temen yang di soreang meninggal dan di awal bulan April juga bibi aku yang di soreang meninggal, dan pertengahan bulan april ini nenek aku sedanng di rumah sakit 
+ 
+
+makasih yaa, udah pernah mau nerima aku
+aku bakal tetep doain sasa dari sini
+senang mengenalmu, senang sekali sa
+
+terima kasih sasa udah mengekspresikan perasaan sasa kemaren
+thank u for a wonderful woman I've ever seen
+and thank u for always be a better person together 
+may Allah always bless u, happy terus ya
+
+i will be ur bloom and see u at the aisle, sasa 
+until our next eclipse :)`
+
+let i = 0
+
+function revealText(){
+
+if(i < text.length){
+
+document.getElementById("finalText").innerHTML += text.charAt(i)
+
+i++
+window.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: "smooth"
+});
+
+setTimeout(revealText, 40)
+
+}else{
+
+document.querySelector(".ending").style.opacity = 1
+
+}
+
+}
+
+window.onload = () =>{
+
+document.getElementById("music").play()
+
+setTimeout(()=>{
+document.getElementById("finalText").style.opacity = 1
+revealText()
+},800)
+
+}
--- script.js
+++ script.js
@@ -1,28 +1,32 @@
-const message = `Aku tau mungkin sekarang semuanya sudah terasa berbeda.
+const message = `sasaaa, how's ur day? i hope u are always well :)
+aku tau sekarang semuanya sudah terasa berbeda, dan mungkin kita juga sudah tidak sedekat dulu.
+ 
+aku menulis ini bukan untuk memaksamu kembali, tapi hanya ingin menyampaikan hal yang mungkin sudah lama ingin aku sampaikan.
 
-Mungkin kita sudah tidak sedekat dulu.
+terima kasih ya… aku benar-benar menghargai semua yang pernah kita lewati. aku juga bisa memahami kenapa sasa memilih jalan yang sekarang, dan aku menghormati itu sepenuhnya.
 
-Aku tidak menulis ini untuk memaksamu kembali.
+maaf, sa. kalo selama ini ada sikap atau caraku yang tanpa sadar menyakitimu, membuatmu kecewa, atau terasa membebani. aku menyadari banyak hal, dan itu jadi pelajaran penting buatku ke depannya.
 
-Aku hanya ingin mengatakan sesuatu
-yang mungkin seharusnya sudah aku katakan sejak lama.
+tentang semua kenangan kita, aku menyimpannya sebagai sesuatu yang baik. terima kasih untuk semua waktu, cerita, dan hal-hal kecil yang pernah kita bagi.
 
-Maaf Sa.
+kalo boleh aku minta tolong ke sasa, boleh berdiri sebentar dan pergi ke depan cermin, sebentar aja?
+heii saa, lihat diri sasa di cermin, bukankah sasa sudah sangat hebat? jangan lupa untuk bilang terimakasih sama diri sasa sendiri ya? yang sudah kuat sampai sejauh ini, percayalah semua akan baik baik saja, semua yang sedang sasa usahakan satu persatu pasti akan Tuhan iyakan.
 
-Maaf kalo selama ini ada sikapku
-yang tanpa sadar menyakitimu
-atau membuatmu kecewa.
+aku tulus berharap sasa selalu baik-baik saja, menemukan ketenangan, dan kebahagiaan dengan caramu sendiri.
 
-Terima kasih, terima kasih
-untuk semua waktu,
-cerita,
-dan kenangan yang pernah ada.
+tidak ada yang perlu sasa khawatirkan dari aku ya, sa. aku akan baik baik saja, dan akan melanjutkan langkahku kedepannya.
 
-Aku tetap berharap
-kamu selalu baik-baik saja
-dan menemukan kebahagiaanmu sendiri.
+terima kasih sudah pernah hadir dalam hidupku.
+senang mengenalmu, senang sekali sa
 
-jaga kesehatannya ya, Have a peaceful day, Sasaa!`
+terima kasih sasa udah mengekspresikan perasaan sasa kemaren
+thank u for a wonderful woman I've ever seen and thank u for always be a better person together
+may Allah always bless u, happy terus ya 
+i will be ur bloom and see u at the aisle, sasa
+until our next eclipse :)
+
+jaga kesehatanmu selalu ya. have a peaceful day, sasaa.
+`
 
 let i = 0
 const speed = 60
